7e92a7ec11e67735a628253bf0beed761bb53a6e
kirill221100 <kirulkacool@gmail.com>
2023-10-22 06:31:38 +0700

    fix

--- core/backend/utils/photo.py
+++ core/backend/utils/photo.py
@@ -1,16 +1,8 @@
 import aiohttp
 from io import BytesIO
-from aiogram.types import Message
 from core.config import get_config
 from aiogram.client.bot import Bot
 from aiogram.types import PhotoSize
-
-
-# async def download_photos(photos: list):
-#     result = []
-#     path = BytesIO()
-#     await photo[-1].download(destination_file=path)
-#     return photo[-1].file
 
 
 async def upload_photo(photo_size: PhotoSize, bot: Bot):
@@ -22,6 +14,6 @@ async def upload_photo(photo_size: PhotoSize, bot: Bot):
                        await bot.download_file(photo_path, destination=destination),
                        content_type='multipart/form-data')
         async with session.post(
-                f'https://api.imgbb.com/1/upload?expiration=600&key={get_config(".env").API_KEY}', data=data
+                f'https://api.imgbb.com/1/upload?key={get_config(".env").API_KEY}', data=data
         ) as response:
             return (await response.json())['data']['url']
