7e1b13f48029b3ffeaf48bb0a91028cac5b1c1f3
kirill221100 <kirulkacool@gmail.com>
2023-10-21 13:12:03 +0700

    photos change

--- core/backend/db/utils/product.py
+++ core/backend/db/utils/product.py
@@ -2,17 +2,19 @@ from core.backend.db.models.product import Product
 from core.backend.db.utils.category import get_category_by_title
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from core.backend.utils.photo import upload_photos
 from aiogram.types import Message
 from typing import List
 from io import BytesIO
 
 
 async def add_product(message_type: str, title: str, description: str, price: int, category_title: str,
-                      session: AsyncSession, photo: str):
+                      session: AsyncSession, photos: list):
     category = await get_category_by_title(category_title, session)
     if category:
+        uploaded_photos = await upload_photos(photos)
         new_product = Product(message_type=message_type, title=title, description=description, price=price,
-                              photo=photo, category=category)
+                              photos=uploaded_photos, category=category)
         session.add(new_product)
         await session.commit()
         return new_product
--- core/backend/utils/photo.py
+++ core/backend/utils/photo.py
@@ -4,16 +4,21 @@ from aiogram.types import Message
 from core.config import get_config
 
 
-async def download_photo(photo: Message.photo):
-    path = BytesIO()
-    await photo[-1].download(destination_file=path)
-    return photo[-1].file
+# async def download_photos(photos: list):
+#     result = []
+#     path = BytesIO()
+#     await photo[-1].download(destination_file=path)
+#     return photo[-1].file
 
 
-async def upload_photo(photo):
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-                f'http://freeimage.host/api/1/upload/?key={get_config(".env").API_KEY}&source={photo}&format=json'
-        ) as response:
-            return await response.json()
+async def upload_photos(photos: list):
+    result = []
+    for photo in photos:
+        path = BytesIO()
+        await photo.download(destination_file=path)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                    f'http://freeimage.host/api/1/upload/?key={get_config(".env").API_KEY}&source={photo.file}&format=json'
+            ) as response:
+                result.append((await response.json())['image']['url'])
+    return result
