4c76929ca87bcff016e4b8b825b92a9a937f6e70
kirill221100 <kirulkacool@gmail.com>
2023-10-22 16:16:41 +0700

    lot

--- core/backend/db/utils/money_lottery.py
+++ core/backend/db/utils/money_lottery.py
@@ -1,6 +1,7 @@
 from core.backend.db.models.lottery import MoneyLottery
 from core.backend.db.utils.user import get_user_by_id
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from sqlalchemy import select
 from random import choice
 
@@ -14,6 +15,10 @@ async def create_money_lottery(price: int, money_prize: int, session: AsyncSessi
 
 async def get_money_lottery_by_id(lottery_id: int, session: AsyncSession):
     return (await session.execute(select(MoneyLottery).filter(MoneyLottery.id == lottery_id))).scalar_one_or_none()
+
+
+async def get_money_lottery_by_id_with_user(lottery_id: int, session: AsyncSession):
+    return (await session.execute(select(MoneyLottery).filter(MoneyLottery.id == lottery_id).options(selectinload(MoneyLottery.users)))).scalar_one_or_none()
 
 
 async def delete_money_lottery(lottery_id: int, session: AsyncSession):
@@ -30,7 +35,7 @@ async def get_users_of_money_lottery_by_id(lottery_id: int, session: AsyncSessio
 
 async def add_user_to_money_lottery(tg_id: int, lottery_id: int, session: AsyncSession):
     user = await get_user_by_id(tg_id, session)
-    money_lottery = await get_money_lottery_by_id(lottery_id, session)
+    money_lottery = await get_money_lottery_by_id_with_user(lottery_id, session)
     if user in money_lottery.users:
         raise Exception('вы уже учавствуете в лотерее')
     if user.balance >= money_lottery.price:
