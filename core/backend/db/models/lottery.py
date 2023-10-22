7348fc1c2f6e3e06fcaf0453ffd78e6e59992358
kirill221100 <kirulkacool@gmail.com>
2023-10-22 15:41:25 +0700

    lottery fix

--- core/backend/db/models/lottery.py
+++ core/backend/db/models/lottery.py
@@ -1,6 +1,22 @@
 from core.backend.db.db_setup import Base
-from sqlalchemy import Column, Integer, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, Table
 from sqlalchemy.orm import relationship
+
+
+product_lotteries_association = Table(
+    "product_lotteries_association",
+    Base.metadata,
+    Column("product_lottery_id", ForeignKey("product_lotteries.id"), primary_key=True),
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+)
+
+
+money_lotteries_association = Table(
+    "money_lotteries_association",
+    Base.metadata,
+    Column("money_lottery_id", ForeignKey("money_lotteries.id"), primary_key=True),
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+)
 
 
 class ProductLottery(Base):
@@ -10,7 +26,7 @@ class ProductLottery(Base):
     balance = Column(Integer, default=0)
     product_id = Column(Integer, ForeignKey('products.id'))
     product = relationship('Product')
-    users = relationship('User')
+    users = relationship('User', secondary=product_lotteries_association)
 
 
 class MoneyLottery(Base):
@@ -19,4 +35,4 @@ class MoneyLottery(Base):
     price = Column(Integer)
     balance = Column(Integer, default=0)
     money_prize = Column(Integer)
-    users = relationship('User')
+    users = relationship('User', secondary=money_lotteries_association)
